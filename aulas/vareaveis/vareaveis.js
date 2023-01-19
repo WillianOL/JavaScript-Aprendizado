1894f0fbd98e5f8cc81e5910b44c91a4497e5d01
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-19 15:15:34 -0300

    Atualizações

---
+++ aulas/vareaveis/vareaveis.js
@@ -0,0 +1,9 @@
+//Responsaveis por armazenar dados na memória. Ex: var, let e const
+
+var nome = 'Willian';
+let idade = 18;
+const Solteiro = true;
+
+console.log(nome, idade, Solteiro);
+
+//Evitam repetições desnecessárias!
