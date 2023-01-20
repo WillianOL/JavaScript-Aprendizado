1855740b19d1f209acfab84c4c659f22b27cab53
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-20 16:53:11 -0300

    Atualizações de exercicios

---
+++ aulas/2-tiposDeDados/tiposDeDados.js
@@ -0,0 +1,22 @@
+//Verificar o tipo de dado de uma vareável, usando o "typeof"
+var nome = 'Willian';
+var ano = 2004;
+var cidade;
+console.log(typeof ano);//Retornando number
+console.log(typeof nome); //Retornando string
+console.log(typeof cidade); //Retornando undefined
+
+//Se o dado for null, é do tipo object
+var comida = null;
+console.log(typeof comida);
+
+//Concatenação de strings
+var nome = 'Willian';
+var sobrenome = 'Oliveira';
+var nomeCompleto = nome + " " + sobrenome; //Concatenando usando o sinal de soma "+"
+console.log(nomeCompleto);
+
+//Somando string e número
+var km = 20;
+console.log('Você estava a uma velocidade de ' + km + 'km/h');
+//*Sempre que somamos uma string e um número, o resultado final sempre vai ser uma string.*
