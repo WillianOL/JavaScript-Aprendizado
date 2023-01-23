54933ddae9d45ba8ef91d0d9c20520600050076e
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-23 09:08:33 -0300

    Update ex02-tiposDeDados.js

--- exercicios/ex02-tiposDeDados.js
+++ exercicios/ex02-tiposDeDados.js
@@ -1,15 +1,21 @@
 // Declare uma variável contendo uma string
 var cidade = 'Alagoas'
 
-// Declare uma variável contendo um número dentro de uma string
-var numero = 'Idade' + 28;
+// Declare uma variável contendo um númer o dentro de uma string
+var numero = '28';
 
 // Declare uma variável com a sua idade
-
+var idade = 18;
 
 // Declare duas variáveis, uma com seu nome
 // e outra com seu sobrenome e some as mesmas
+var nome = 'Willian', sobrenome = 'Oliveira';
+var nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto)
 
 // Coloque a seguinte frase em uma variável: It's time
+var frase = "It´s time"
+console.log(frase);
 
 // Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
