c3fb8bfe0f4aed83fa72d871b9f672170caf3415
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-23 09:48:27 -0300

    Atualizações

---
+++ aulas/3-números-e-Operadores/numerosOperadores.js
@@ -0,0 +1,13 @@
+//Valores númericos sem estar em "" são números.
+var idade = 18
+var exp = 3e5 //exponencial - 300000
+var ponto = 2.57 //usar ponto e não vírgula.
+
+//Opreadores aritiméticos
+var s = 10 + 2 // 12
+var sub = 10 - 2 // 8
+var mult = 10 * 2 // 20
+var divi = 10 / 2 // 5
+var exp = 10 ** 2 // 100
+//resto da divisão
+var modulo = 10 % 3 // 1
