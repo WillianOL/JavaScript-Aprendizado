09afe5ecc083a2f8c573a3e3a70928469d88faff
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-23 15:32:19 -0300

    Create ex03-tipos-de-dados-2.js

---
+++ exercicios/ex03-tipos-de-dados-2.js
@@ -0,0 +1,28 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+/*
+Primeiro vai sar feita a multiplicação e depois a divisão, com o resultado sendo 5. Depois vai ser feita as somas e sendo assim o resultado 35
+*/
+
+// Crie duas expressões que retornem NaN
+var expre1 = 'Ola' / 2
+var expre2 = 'frase' - 2
+console.log(expre1, expre2)
+
+// Somar a string '200' com o número 50 e retornar 250
+var texto = '200';
+var soma = +texto + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var num = 5
+num++
+console.log(num);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso;
+
+console.log(pesoPorDois);
