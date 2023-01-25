4e494c7f07a0a7d57a87ff809dd765e85934a9d6
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-25 10:21:21 -0300

    Atualizações

---
+++ exercicios/ex04-boolean-e-condicionais.js
@@ -0,0 +1,51 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var idadeParente = 32;
+var minhaIdade = 33;
+
+if( minhaIdade > idadeParente){
+  console.log('É maior')
+} else if( minhaIdade === idadeParente){
+  console.log('É igual')
+} else{
+  console.log('É menor')
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(`É igual a 3 = ${expressao}`)
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre'; // truthy
+var idade = 28; // truthy
+var possuiDoutorado = false; // Falsy
+var empregoFuturo; // Falsy
+var dinheiroNaConta = 0; // Falsy
+
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta)
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if(brasil > china){
+  console.log('Brasil tem mais habitantes');
+} else{
+  console.log('China tem mais habitantes');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+//Vai aparecer falso
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
+//Vai aparcer 'Cão'
