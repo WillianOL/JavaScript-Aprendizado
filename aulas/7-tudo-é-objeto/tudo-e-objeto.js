a6e520471d35b5a698babf6073032dc09f384695
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-30 19:05:33 -0300

    Atualizações

--- aulas/7-tudo-é-objeto/tudo-e-objeto.js
+++ aulas/7-tudo-é-objeto/tudo-e-objeto.js
@@ -7,3 +7,9 @@ console.log(nome.toLowerCase())
 var n1 = 1.6
 console.log(n1.toFixed())
 console.log(n1.toString())
+
+// Elementos DOM
+var button = document.querySelector('.btn') //Acessando a tag 'a' atravéz de seu seletor CSS.
+button.addEventListener('click', function(){
+    return console.log('Ola');
+})
