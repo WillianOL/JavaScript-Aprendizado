e3710471f9824bf50f2eecc3d4ca51832406a2d9
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-30 19:00:28 -0300

    Update tudo-e-objeto.js

--- aulas/7-tudo-é-objeto/tudo-e-objeto.js
+++ aulas/7-tudo-é-objeto/tudo-e-objeto.js
@@ -3,5 +3,7 @@ var nome = 'Willian';
 console.log(nome.toLowerCase())
 // Transformando a vareáel (objeto) em letras minúsculas.
 
+// Para números também
 var n1 = 1.6
 console.log(n1.toFixed())
+console.log(n1.toString())
