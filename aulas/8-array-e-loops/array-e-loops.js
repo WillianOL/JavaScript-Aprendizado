ce29809be28f3530be5f842dcd67b6c4415abaa7
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-31 09:22:16 -0300

    Atualizações

---
+++ aulas/8-array-e-loops/array-e-loops.js
@@ -0,0 +1,12 @@
+// Array é um grupo de valores geralmente relacionados
+var celulares = ['xiaomi', 'samsung', 'iphone'];
+console.log(celulares);
+
+var exemplo01 = celulares.pop(); // Remove o ultimo item e retorna ele.
+celulares.push('LG'); // Adiciona o elemento no ultimo indicie
+console.log(celulares.length); // Número de elementos/itens
+
+//For loop - estrutura de repetição
+for(var contagem = 0; contagem <= 10; contagem++){
+    console.log(contagem);
+} //Primeiro cria-se uma vareável de contagem, depois a condição para que o loop continue. Depois que ele executar o código, faz o incremento de +1 até que a condição seja falsa e o loop pare.
