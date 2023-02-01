9b7899ae8673d0f6dc75df6b2a8394b3268da67e
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-01 19:56:37 -0300

    Atualizações

--- aulas/11-DOM-Iniciante/O-que-é-DOM/DOM.js
+++ aulas/11-DOM-Iniciante/O-que-é-DOM/DOM.js
@@ -1,3 +1,5 @@
 // DOM
 const href = location.href;
 console.log(href); // usando o objeto "location" e usando sua propriedade "href" para acessar o link do site
+
+const titulo = document.querySelector('h2');
