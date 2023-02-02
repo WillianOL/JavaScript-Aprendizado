378d56feedcfd7fb40ce126de1800ad55bdbd6f3
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-02 13:30:10 -0300

    Atualizações

---
+++ exercicios/ex011-DOM/ex11-DOM01.js
@@ -0,0 +1,14 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elemento = document.querySelector('.ativo')
+console.log(elemento);
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
+const janela = document.body.offsetWidth
+console.log(janela);
