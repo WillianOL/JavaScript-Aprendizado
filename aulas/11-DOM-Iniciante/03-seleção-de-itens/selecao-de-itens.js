eb904becb32417f36068966e86f6786e80e48001
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-03 13:42:41 -0300

    Update selecao-de-itens.js

--- aulas/11-DOM-Iniciante/03-seleção-de-itens/selecao-de-itens.js
+++ aulas/11-DOM-Iniciante/03-seleção-de-itens/selecao-de-itens.js
@@ -1,5 +1,8 @@
-const animaisSection = document.getElementById('animais') // Selecionando a section por ID
+const animaisSection = document.getElementById('animais'); // Selecionando a section por ID
 //caso o elemento não exista, vai retornar 'null'
 
-const gridSection = document.getElementsByClassName('grid-section')
-console.log(gridSection);
+const gridSection = document.getElementsByClassName('grid-section'); // Selecionando a gridSection por Class
+
+const listaAnimais = document.querySelector('.menu ul');
+const itenLista = listaAnimais.querySelector('li')[2];
+console.log(itenLista);
