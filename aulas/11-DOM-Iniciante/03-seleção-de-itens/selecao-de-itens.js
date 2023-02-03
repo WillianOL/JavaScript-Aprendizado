07688e016d4e0b574d9641582b2553892a565457
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-03 13:17:47 -0300

    Atualizações

---
+++ aulas/11-DOM-Iniciante/03-seleção-de-itens/selecao-de-itens.js
@@ -0,0 +1,5 @@
+const animaisSection = document.getElementById('animais') // Selecionando a section por ID
+//caso o elemento não exista, vai retornar 'null'
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection);
