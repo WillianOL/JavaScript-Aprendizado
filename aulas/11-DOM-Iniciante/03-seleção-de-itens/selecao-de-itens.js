ab3a79078a78da1aac06a71a33d8f91a8731e32a
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-03 13:44:23 -0300

    Update selecao-de-itens.js

--- aulas/11-DOM-Iniciante/03-seleção-de-itens/selecao-de-itens.js
+++ aulas/11-DOM-Iniciante/03-seleção-de-itens/selecao-de-itens.js
@@ -3,6 +3,7 @@ const animaisSection = document.getElementById('animais'); // Selecionando a sec
 
 const gridSection = document.getElementsByClassName('grid-section'); // Selecionando a gridSection por Class
 
+//Usando o querySelector, ele sempre vai pegar o primeiro elemento de acordo com seu seletor CSS.
 const listaAnimais = document.querySelector('.menu ul');
-const itenLista = listaAnimais.querySelector('li')[2];
+const itenLista = listaAnimais.querySelector('li');
 console.log(itenLista);
