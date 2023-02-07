dc900230cf781381a8facd7e27136304781f0446
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-07 13:48:36 -0300

    Update classes-e-atributos.js

--- exercicios/ex011-DOM/classes-e-atributos/classes-e-atributos.js
+++ exercicios/ex011-DOM/classes-e-atributos/classes-e-atributos.js
@@ -1,8 +1,20 @@
 // Adicione a classe ativo a todos os itens do menu
 const menuItens = document.querySelectorAll('.menu a');
-
+menuItens.forEach((index) => {
+    index.classList.add('ativo')
+})
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+menuItens.forEach((item) => {
+    item.classList.remove('ativo')
+})
+
+menuItens[0].classList.add('ativo')
 
 // Verifique se as imagens possuem o atributo alt
+const imagens = document.querySelectorAll('img')
+imagens.forEach((itens) => {
+    const possuiAlt = itens.hasAttribute('alt')
+    console.log(possuiAlt);
+})
 
 // Modifique o href do link externo no menu
