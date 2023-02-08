894f0ef8d88651fb865280fbfe8be9db13362813
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-08 18:24:03 -0300

    Atualizações

---
+++ aulas/11-DOM-Iniciante/07-eventos/eventos.js
@@ -0,0 +1,22 @@
+const img = document.querySelector('img')
+// Interagindo com elementos através de eventos.
+img.addEventListener('click', () => {
+    console.log('clicou na imagem');
+}); // Neste caso, há uma função com um evento de click. Quando o click acontecer na imagem vai disparar a função.
+
+// Callback
+//Nomear seu callback(nome da função) da maneira que quiser
+const img2 = document.querySelector('.img2')
+function clicarImagem(){
+    console.log('Clicou na imagem 02');
+}
+
+img2.addEventListener('click', clicarImagem);
+
+//Parâmetro
+const pagina = document.querySelector('body')
+function clicarBody(event){
+    const Y = event.
+}
+
+pagina.addEventListener('click', clicarBody);
