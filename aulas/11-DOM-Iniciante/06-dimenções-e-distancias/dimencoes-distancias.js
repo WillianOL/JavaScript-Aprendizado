5317d8be61af6b8de5ebcb057c3134fa7b4152fa
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-08 10:19:32 -0300

    Update dimencoes-distancias.js

--- aulas/11-DOM-Iniciante/06-dimenções-e-distancias/dimencoes-distancias.js
+++ aulas/11-DOM-Iniciante/06-dimenções-e-distancias/dimencoes-distancias.js
@@ -5,3 +5,9 @@ console.log(listaAltura);
 const listaAlturaTotal = listaAnimais.scrollHeight; // Heigth total do elemento, mesmo dentro de um scroll.
 console.log(listaAlturaTotal);
 
+const faq = document.getElementById('faq');
+console.log(faq.offsetTop); // Distancia entre o topo do elemento e o topo da página.
+
+const clientRect = faq.getBoundingClientRect();
+console.log(clientRect);
+
