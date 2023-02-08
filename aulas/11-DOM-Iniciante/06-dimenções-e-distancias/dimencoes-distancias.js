4e2e9e1b015becd9e706e048e4c44f2ba6be73fb
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-08 10:00:05 -0300

    Update dimencoes-distancias.js

--- aulas/11-DOM-Iniciante/06-dimenções-e-distancias/dimencoes-distancias.js
+++ aulas/11-DOM-Iniciante/06-dimenções-e-distancias/dimencoes-distancias.js
@@ -2,6 +2,6 @@ const listaAnimais = document.querySelector('.listaAnimais');
 
 const listaAltura = listaAnimais.clientHeight; // height + padding
 console.log(listaAltura);
-
-const listaAlturaTotal = listaAnimais.scrollHeight;
+const listaAlturaTotal = listaAnimais.scrollHeight; // Heigth total do elemento, mesmo dentro de um scroll.
 console.log(listaAlturaTotal);
+
