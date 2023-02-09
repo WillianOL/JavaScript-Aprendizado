df72e1fbd744de78af527fa3ad93acbc8fe5b100
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-09 18:46:30 -0300

    atualizações

---
+++ exercicios/ex011-DOM/eventos/eventos.js
@@ -0,0 +1,27 @@
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a');
+
+function clicouLink(event){
+    event.preventDefault();
+    linksInternos.forEach((itens, index) => {
+        itens.classList.toggle('ativo')
+    })
+}
+
+linksInternos.forEach((links) => {
+    links.addEventListener('click', clicouLink);
+})
+
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
