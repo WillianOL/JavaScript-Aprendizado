47df82ac9505bcb09db0f66fa93818bd3ed821c5
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-09 19:07:07 -0300

    Update eventos.js

--- exercicios/ex011-DOM/eventos/eventos.js
+++ exercicios/ex011-DOM/eventos/eventos.js
@@ -4,11 +4,13 @@
 // o comportamento padrão desses links
 const linksInternos = document.querySelectorAll('a');
 
-function clicouLink(event){
+function clicouLink(event) {
     event.preventDefault();
-    linksInternos.forEach((itens, index) => {
-        itens.classList.toggle('ativo')
+    linksInternos.forEach((itens) => {
+        itens.classList.remove('ativo')
     })
+
+    event.target.classList.toggle('ativo')
 }
 
 linksInternos.forEach((links) => {
@@ -19,9 +21,30 @@ linksInternos.forEach((links) => {
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
 
+function cliqueAleatorio(event){
+    const clique = event.target
+    console.log(clique);
+}
+window.addEventListener('click', cliqueAleatorio)
 
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
+function cliqueAleatorioRemove(event){
+    const clique = event.target;
+    clique.remove();
+}
 
+window.addEventListener('click', cliqueAleatorioRemove)
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site. 
+const texto = document.querySelectorAll('p');
+
+function aumentarTexto(event){
+    if(event.key === 't'){
+        texto.forEach((item) => {
+            item.style.fontSize = '20px'
+        });
+    }
+}
+
+window.addEventListener('keydown', aumentarTexto);
