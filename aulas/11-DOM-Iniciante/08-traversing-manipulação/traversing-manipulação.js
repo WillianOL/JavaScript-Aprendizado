5ecbdb5fd46bba6131de8db4c6442ce8d7d2cb0e
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-10 13:37:07 -0300

    Update traversing-manipulação.js

---
+++ aulas/11-DOM-Iniciante/08-traversing-manipulação/traversing-manipulação.js
@@ -0,0 +1,8 @@
+// Transversing
+const listaAnimais = document.querySelector('.listaAnimais')
+
+const ex01 = listaAnimais.parentElement; // Pai do elemento
+const ex02 = listaAnimais.parentElement.parentElement; // Pai do pai do elemento
+const ex03 = listaAnimais.previousElementSibling; // Elemento acima do selecionado
+const ex04 = listaAnimais.nextElementSibling; // Elemento abaixo do selecionado
+console.log(ex04);
