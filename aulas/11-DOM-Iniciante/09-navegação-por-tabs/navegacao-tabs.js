0c3748b9f90c671ea58832dae43b3584aa1446b0
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-14 09:03:22 -0300

    Update navegacao-tabs.js

--- aulas/11-DOM-Iniciante/09-navegação-por-tabs/navegacao-tabs.js
+++ aulas/11-DOM-Iniciante/09-navegação-por-tabs/navegacao-tabs.js
@@ -1,16 +1,19 @@
 const menuClick = document.querySelectorAll('.js-listaAnimais img');
 const descricaoAnimais = document.querySelectorAll('.js-descricao-tabs section');
-descricaoAnimais[0].classList.add('ativado');
+descricaoAnimais[0].classList.add('ativado'); // Adiciona o conteúdo para não ficar sem nada inicialmente
 
-function mostrarInfo(index) {
-    descricaoAnimais.forEach((itens) => {
-        itens.classList.remove('ativado');
+if(menuClick.length && descricaoAnimais.length){ // Só vai executar se os dois elementos existirem na tela.
+    
+    function mostrarInfo(index) { // função recebendo o index do forEach para adicionar a classe a section de acordo com a img.
+        descricaoAnimais.forEach((itens) => { // Remove a classe 'ativado' das demais se tiverem
+            itens.classList.remove('ativado');
+        });
+        descricaoAnimais[index].classList.add('ativado'); // Adiciona a classe a section de acordo com o index da imagem clicada.
+    }
+    
+    menuClick.forEach((imgs, index) => { // Criando a função de click para todas as imagens.
+        imgs.addEventListener('click', () => {
+            mostrarInfo(index);
+        });
     });
-    descricaoAnimais[index].classList.add('ativado');
 }
-
-menuClick.forEach((imgs, index) => {
-    imgs.addEventListener('click', () => {
-        mostrarInfo(index);
-    });
-});
