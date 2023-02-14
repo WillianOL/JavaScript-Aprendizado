7a8e5d0f4dc300ca896b8e68725b398bb1be8bbf
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-14 08:54:47 -0300

    atualizações

--- aulas/11-DOM-Iniciante/09-navegação-por-tabs/navegacao-tabs.js
+++ aulas/11-DOM-Iniciante/09-navegação-por-tabs/navegacao-tabs.js
@@ -1,18 +1,16 @@
-const tabMenu = document.querySelectorAll('.js-listaAnimais img');
-const animaisText = document.querySelectorAll('.js-descricao-tabs section');
-animaisText[0].classList.add('ativado') // Quando o site carregar, sempre deixar uma descrição ativada
+const menuClick = document.querySelectorAll('.js-listaAnimais img');
+const descricaoAnimais = document.querySelectorAll('.js-descricao-tabs section');
+descricaoAnimais[0].classList.add('ativado');
 
-
-function mostrarDescricao(i){
-    animaisText.forEach((descricao) => { // Remove das demais se tiverem
-        descricao.classList.remove('ativado')
+function mostrarInfo(index) {
+    descricaoAnimais.forEach((itens) => {
+        itens.classList.remove('ativado');
     });
-
-    animaisText[i].classList.add('ativado') // adiciona a que foi clicada através do index no forEach.
+    descricaoAnimais[index].classList.add('ativado');
 }
 
-tabMenu.forEach((item, index) => { // adiciona a função atravéz do evento de click a cada imagem, com o index como parâmetro.
-    item.addEventListener('click', () => {
-        mostrarDescricao(index);
-    })
-})
+menuClick.forEach((imgs, index) => {
+    imgs.addEventListener('click', () => {
+        mostrarInfo(index);
+    });
+});
