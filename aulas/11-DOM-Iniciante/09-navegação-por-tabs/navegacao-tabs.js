ad9f1a01836e7e20892bfdec94b151b43e529734
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-15 19:55:40 -0300

    Update navegacao-tabs.js

--- aulas/11-DOM-Iniciante/09-navegação-por-tabs/navegacao-tabs.js
+++ aulas/11-DOM-Iniciante/09-navegação-por-tabs/navegacao-tabs.js
@@ -23,16 +23,15 @@ initDescricao();
 
 function initInfo() {
     const faqBtns = document.querySelectorAll('.js-showAnimais h2');
-    const showP = 'showP'
+    const showP = 'showP' // Definindo a classe CSS em uma constante para uma futura modificação
 
-    faqBtns[0].classList.add(showP);
-    faqBtns[0].nextElementSibling.classList.add(showP);
+    faqBtns[0].nextElementSibling.classList.add(showP); // Deixa o primeiro item visível na tela.
 
-    function mostrarInfoFaq(){
+    function mostrarInfoFaq(){ // função para clicar e adicionar a classe ao item abaixo do clicado.
     this.nextElementSibling.classList.toggle(showP)
     }
 
-    faqBtns.forEach((item) => {
+    faqBtns.forEach((item) => { // Crianco a função com evento de click a todos os h2
     item.addEventListener('click', mostrarInfoFaq);
     })
 }
