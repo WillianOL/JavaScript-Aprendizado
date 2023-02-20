b0d7e836a5d612a6be7485c7da056a6f847f7e63
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-20 09:50:02 -0300

    Update objetos.js

--- aulas/12.constructor-functions/objetos.js
+++ aulas/12.constructor-functions/objetos.js
@@ -6,4 +6,16 @@ const carro = {
 const fiat = carro;
 fiat.marca = 'Fiat';
 fiat.preco = 40000; // Dessa forma, a constante vai substituir os valores do objeto carro. Não dando para reaproveitar ele.
+console.log(fiat);
 
+// Constructor function
+function Carro() { // Iniciar constructor functions com letras maiúscolas (pascal case)
+}
+
+const honda = new Carro();  
+honda.marca = 'Honda';
+honda.preco = 50000;
+
+const ferrari = new Carro();
+ferrari.marca = 'Ferrari'
+ferrari.preco = 100000
