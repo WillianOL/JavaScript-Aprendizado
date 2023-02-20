134a2077278eb26551f51144f34e994abc1c9b27
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-20 14:10:20 -0300

    atualizações

---
+++ exercicios/ex012-constructorFunction/ConstructorFunction.js
@@ -0,0 +1,39 @@
+// Transforme o objeto abaixo em uma Constructor Function
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function (){
+      console.log(this.nome + ' andou');
+    }
+  }
+
+  const pessoa = new Pessoa('Willian', 18);
+
+  
+  // Crie 3 pessoas, João - 20 anos,
+  // Maria - 25 anos, Bruno - 15 anos
+  const pessoa01 = new Pessoa('João', 20)
+  const pessoa02 = new Pessoa('Maria', 25)
+  const pessoa03 = new Pessoa('Bruno', 15)
+  
+  // Crie uma Constructor Function (Dom) para manipulação
+  // de listas de elementos do dom. Deve conter as seguintes
+  // propriedades e métodos:
+  //
+  // elements, retorna NodeList com os elementos selecionados
+  // addClass(classe), adiciona a classe a todos os elementos
+  // removeClass(classe), remove a classe a todos os elementos
+
+function Dom(seletor, classe) {
+    this.elements = function() {
+        return document.querySelectorAll(seletor)
+    }
+
+    this.AddClass = function() {
+        this.elements.foreach((itens) => {
+            itens.classList.add(classe)
+        })
+    }
+}
+
+const seletor = new Dom('li', 'ativado')
