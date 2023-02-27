1e44d92844b0855bdbcb1214ba83332ab5d6317f
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-27 14:23:48 -0300

    Update ConstructorFunction.js

--- exercicios/ex012-constructorFunction/ConstructorFunction.js
+++ exercicios/ex012-constructorFunction/ConstructorFunction.js
@@ -1,19 +1,15 @@
 // Transforme o objeto abaixo em uma Constructor Function
-function Pessoa(nome, idade) {
-    this.nome = nome;
-    this.idade = idade;
-    this.andar = function (){
+const pessoa = {
+    nome: 'Nome pessoa',
+    idade: 0,
+    andar() {
       console.log(this.nome + ' andou');
     }
   }
-
-  const pessoa = new Pessoa('Willian', 18);
-
+  
   // Crie 3 pessoas, João - 20 anos,
   // Maria - 25 anos, Bruno - 15 anos
-  const pessoa01 = new Pessoa('João', 20)
-  const pessoa02 = new Pessoa('Maria', 25)
-  const pessoa03 = new Pessoa('Bruno', 15)
+  
   
   // Crie uma Constructor Function (Dom) para manipulação
   // de listas de elementos do dom. Deve conter as seguintes
@@ -22,19 +18,3 @@ function Pessoa(nome, idade) {
   // elements, retorna NodeList com os elementos selecionados
   // addClass(classe), adiciona a classe a todos os elementos
   // removeClass(classe), remove a classe a todos os elementos
-function Dom(seletor, classe) {
-    const listaElements = document.querySelectorAll(seletor);
-    this.elements = listaElements;
-    this.addClass = function(){
-            listaElements.forEach((itens) => {
-            itens.classList.add(classe)
-        })
-    }
-    this.removeClass = function() {
-        listaElements.forEach(itens => {
-            itens.classList.remove(classe)
-        }) 
-    }
-}
-
-const seletor = new Dom('li', 'ativado')
