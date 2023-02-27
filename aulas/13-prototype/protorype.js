e4a25fce53407fa264896c7cd6880ea65bd16d27
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-27 18:15:51 -0300

    Update protorype.js

--- aulas/13-prototype/protorype.js
+++ aulas/13-prototype/protorype.js
@@ -1,4 +1,4 @@
-// Prototype sempre está relacionado a funções
+// Prototype sempre está ligado a funções.
 function Bolo(saborAt, precoAt) {
     this.sabor = saborAt;
     this.preco = precoAt;
@@ -7,3 +7,14 @@ const carro01 = new Bolo('Morango', 60)
 
 console.log(carro01.prototype); // undefined, pois carro01 é um objeto e não uma função
 console.log(Bolo.prototype); // Retorna o objeto
+
+function Pessoa(nomeAt, idadeAt){
+    this.nome = nomeAt;
+    this.idade = idadeAt;
+}
+
+Pessoa.prototype.andar = function (){ // Como se estivesse adicionando uma nova propriedade a constructor function.
+    return this.nome + ' andou'
+}
+
+const willian = new Pessoa('Willian', 18)
