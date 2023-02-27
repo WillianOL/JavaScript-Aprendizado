feb882b43d683a2edd2b9dfd297cce641565a846
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-27 14:56:31 -0300

    Update ConstructorFunction.js

--- exercicios/ex012-constructorFunction/ConstructorFunction.js
+++ exercicios/ex012-constructorFunction/ConstructorFunction.js
@@ -3,44 +3,46 @@ const pessoa = {
     nome: 'Nome pessoa',
     idade: 0,
     andar() {
-      console.log(this.nome + ' andou');
+    console.log(this.nome + ' andou');
     }
-  }
+}
 
-function Pessoa(){
-    this.nome = 'Nome pessoa'
-    this.idade = 0
+function Pessoa(nome, idade){
+    this.nome = nome
+    this.idade = idade
     this.andar = function() {
-      console.log(this.nome + ' andou');
+    console.log(this.nome + ' andou');
     }
-  }
-  
-  // Crie 3 pessoas, João - 20 anos,
-  // Maria - 25 anos, Bruno - 15 anos
-  const pessoa01 = {nome: 'João', idade: 20,}
-  const pessoa02 = {nome: 'Maria', idade: 25,}
-  const pessoa03 = {nome: 'Bruno', idade: 15,}
-  
-  // Crie uma Constructor Function (Dom) para manipulação
-  // de listas de elementos do dom. Deve conter as seguintes
-  // propriedades e métodos:
-  //
-  // elements, retorna NodeList com os elementos selecionados
-  // addClass(classe), adiciona a classe a todos os elementos
-  // removeClass(classe), remove a classe a todos os elementos
+}
 
-  function Dom(element){
-    this.elements = document.querySelectorAll(element);
-    this.AddClass = () => {
-        this.elements.forEach((itens) => {
-            itens.classList.add('classe')
-        })
-    }
-    this.removeClass = () => {
-        this.elements.forEach((itens) => {
-            itens.classList.remove('classe')
-        })
-    }
-  }
 
-  const lista = new Dom('li')
+  
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+const pessoa01 = {nome: 'João', idade: 20,}
+const pessoa02 = {nome: 'Maria', idade: 25,}
+const pessoa03 = {nome: 'Bruno', idade: 15,}
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
+
+function Dom(element){
+this.elements = document.querySelectorAll(element);
+this.AddClass = () => {
+    this.elements.forEach((itens) => {
+        itens.classList.add('classe')
+    })
+}
+this.removeClass = () => {
+    this.elements.forEach((itens) => {
+        itens.classList.remove('classe')
+    })
+}
+}
+
+const lista = new Dom('li')
