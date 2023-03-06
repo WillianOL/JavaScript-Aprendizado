0c100e1ee4040a3780a27f20c03b3092656b6228
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-06 18:19:14 -0300

    Update index.js

--- aulas/14-native-host-user/index.js
+++ aulas/14-native-host-user/index.js
@@ -15,4 +15,11 @@ const Pessoa = {
     idade: 18,
 }
 
-// ECMA --> 
+// ECMA --> Organização criada com o intuito de estabelecer um padrão de tecnologias. ECMAScript é o padão do JavaScript
+
+// Verificar se um método existe
+if(typeof Array.from !== 'undefined') {
+    console.log('Método existe.');
+} else {
+    console.log('Não existe')
+}
