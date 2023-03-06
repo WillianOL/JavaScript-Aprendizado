b764499905caaad76e7cf7f71651d48133e5dbe2
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-06 14:24:00 -0300

    Update index.js

--- aulas/14-native-host-user/index.js
+++ aulas/14-native-host-user/index.js
@@ -1,5 +1,10 @@
-// Constutores dos objetos nativos
+// Exemplos de constutores dos objetos nativos
 Object;
 String;
 Array;
 Function;
+// * Implementação da linguagem, independente do host.
+
+// Objetos do host são aqueles implementados pelo próprio ambiente. (Navegadores).
+HTMLCollection;
+NodeList;
