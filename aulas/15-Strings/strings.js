0e9ab6735f6342c6d84c11ac93a4faa73699a08c
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-07 10:18:20 -0300

    Update strings.js

--- aulas/15-Strings/strings.js
+++ aulas/15-Strings/strings.js
@@ -1,3 +1,7 @@
 const nome = 'Willian';
 const comida = new String('Macarrão');
 const ano = new String(2023);
+
+// Métodos e própriedades.
+const length = comida.length // Número de caractéres da string. (espaços tmabém contam)
+console.log(length);
