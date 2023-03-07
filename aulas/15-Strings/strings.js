c8043c017ceccac808df941869c2c255418fd953
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-07 19:33:35 -0300

    Update strings.js

--- aulas/15-Strings/strings.js
+++ aulas/15-Strings/strings.js
@@ -20,3 +20,13 @@ game.includes(gameList) // False
 
 const endsWith = gameList.endsWith('PS1') // true - pois termina com "PS1"
 const startsWith = game.startsWith('X') // true - pois comerça com "X"
+
+const palavra01 = 'Andei por ai'
+const palavra02 = 'Comprei ovos'
+const palavra03 = 'Olá, Dev!'
+
+console.log(palavra02.slice(0, 5)); // mostra os caracteres do index 0 ao 5. 
+console.log(palavra03.slice(2)); // Mostra apartir do index 2.
+console.log(palavra01.slice(-4)) // mostra os 4 ultimos caracteres.
+console.log(palavra02.slice(0, -9)) // tira os ultimos 9 caracteres.
+console.log(palavra01.slice(2, 4)) // mostra os caracteres do 2 até 4.
