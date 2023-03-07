0bb0235ae827b7f973b8375449e6470a6f581236
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-07 09:47:29 -0300

    Update index.js

---
+++ exercicios/ex014-native-host-user/index.js
@@ -0,0 +1,16 @@
+// Liste 5 objetos nativos
+Object;
+Function;
+Array;
+Number;
+String;
+
+// Liste 5 objetos do browser
+HTMLCollection;
+NodeList;
+Element;
+Window;
+Document;
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
