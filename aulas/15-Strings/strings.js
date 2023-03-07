33ec0303b9de996953ab6e503bf8330bcfa6fc58
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-07 18:16:12 -0300

    Update strings.js

--- aulas/15-Strings/strings.js
+++ aulas/15-Strings/strings.js
@@ -6,6 +6,7 @@ const ano = new String(2023);
 const length = comida.length // Número de caractéres da string. (espaços tmabém contam)
 console.log(length);
 
-const frase = 'Hoje eu andei de bike' 
+const frase = 'Meu nome é' 
 console.log(frase.charAt(3)); // Mostra o caracter de acordo com seu indicie
 
+console.log(frase.concat(nome));
