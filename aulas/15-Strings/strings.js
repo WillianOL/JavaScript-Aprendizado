6340d9efe509f1a3941a4ea9721e84dad4c7c26f
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-07 19:58:13 -0300

    Update strings.js

--- aulas/15-Strings/strings.js
+++ aulas/15-Strings/strings.js
@@ -30,3 +30,7 @@ console.log(palavra03.slice(2)); // Mostra apartir do index 2.
 console.log(palavra01.slice(-4)) // mostra os 4 ultimos caracteres.
 console.log(palavra02.slice(0, -9)) // tira os ultimos 9 caracteres.
 console.log(palavra01.slice(2, 4)) // mostra os caracteres do 2 até 4.
+
+const frase02 = 'Fui caminhar pela manhã'
+console.log(frase02.indexOf('pela')); // Retorna o indicie da string selecionada. (primeira que aparecer)
+console.log(frase02.lastIndexOf('a')); // Retorna a ultima string selecionada. No caso, o ultimo "a"
