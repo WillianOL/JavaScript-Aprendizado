c820dbaedd74377f7be74888bd09d4f9fefbcac9
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-10 10:01:03 -0300

    Update strings.js

--- aulas/15-Strings/strings.js
+++ aulas/15-Strings/strings.js
@@ -7,10 +7,10 @@ const length = comida.length // Número de caractéres da string. (espaços tmab
 console.log(length);
 
 const frase = 'Meu nome é ';
-
 const carac = frase.charAt(2); // Mostra o caracter de acordo com seu indicie
 
 const concatena = frase.concat(nome, "."); // Concatena as strings
+
 
 const gameList = 'PS2, GameBoy, Xbox, PS1'
 const game = 'Xbox'
@@ -20,6 +20,7 @@ game.includes(gameList) // False
 
 const terminaCom = gameList.endsWith('PS1') // true - pois termina com "PS1"
 const comecaCom = game.startsWith('X') // true - pois comerça com "X"
+
 
 const palavra01 = 'Eu andei de bike'
 const palavra02 = 'Comprei ovos'
@@ -31,12 +32,13 @@ console.log(palavra01.slice(-4)) // mostra os 4 ultimos caracteres.
 console.log(palavra02.slice(0, -9)) // tira os ultimos 9 caracteres.
 console.log(palavra01.slice(2, 4)) // mostra os caracteres do 2 até 4.
 
+
 const frase02 = 'Fui caminhar pela manhã'
 console.log(frase02.indexOf('pela')); // Retorna o indicie da string selecionada. (primeira que aparecer)
 console.log(frase02.lastIndexOf('a')); // Retorna a ultima string selecionada. No caso, o ultimo "a".
 
-const listaPreco = ['R$9', 'R$99', 'R$199', 'R$1999'];
 
+const listaPreco = ['R$9', 'R$99', 'R$199', 'R$1999'];
 console.log(listaPreco[0].padEnd(5, ',0')); // Da um espaço final da string, preenchendo o espaço vazio com ',0'
 
 listaPreco.forEach((itens) => {
@@ -53,3 +55,7 @@ console.log(preco);
 let listaItens = 'Boné, Camisa, Camisa, Shorts'
 listaItens = listaItens.replace('Camisa', 'Cuécas'); // substituí o primeiro item que ele encontra.
 console.log(listaItens);
+
+// O split faz a separação de itens de acordo com o valor passado e retorna uma Array.
+console.log(listaItens.split(', ')); // No caso, vai quebrar sempre que uma ", " aparecer.
+console.log(listaItens.split(''));
