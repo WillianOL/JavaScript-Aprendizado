5f1f0025fcaaa26a4360c4f37118985150a678e2
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-13 15:28:20 -0300

    Update number-e-math.js

--- aulas/16-number-e-math/number-e-math.js
+++ aulas/16-number-e-math/number-e-math.js
@@ -8,3 +8,12 @@ console.log(Number.isInteger(17.5)); // false - pois é um valor decimal.
 
 // Retorna um número através de uma string
 console.log(Number.parseFloat('135.70')); // 135.7
+console.log(Number.parseFloat('R$135.70')); // NaN - pois ele não consegue transformar nenhum caracter na frente do número.
+console.log(Number.parseInt('197.32')); // transforma em integral, removendo os números decimais.
+
+// Arredonda o valor dependendo do valor das casas decimais.
+const roupa = 59.90
+console.log(roupa.toFixed()); // 60
+
+const casa = 150000.378
+console.log(casa.toFixed(2)); // número de casas após a vírgula arredondado.
