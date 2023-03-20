be175320a8e49acb750a99093a479aa2e9445b90
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-20 10:12:05 -0300

    Update arrays.js

--- aulas/17-array/arrays.js
+++ aulas/17-array/arrays.js
@@ -3,7 +3,7 @@ const arrayDados = ['Olá', new String('PARABÊNS'), [10, {cor: 'Preto', comida:
 
 console.log(arrayDados[0]); // Olá
 console.log(arrayDados[1]); // String {PARABÊNS}
-console.log(arrayDados[2][0]); // 10 - Acessando o primeiro elemento do segundo array dentro dele.
+console.log(arrayDados[2][0]); // 10 - Acessando o primeiro elemento do array dentro dele.
 console.log(arrayDados[2][1].cor); // 'Preto' - acessando o objeto dentro do array.
 console.log(arrayDados[2][1].comida); // 'Bolo'.
 console.log(arrayDados[3]('Acesso negado')); // Acessano e executanto a função.
