7a0c9771e12bd07ba2b8b4330de185460cf011e6
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-22 19:07:13 -0300

    Update array-interação.js

--- aulas/18-array-e-interação/array-interação.js
+++ aulas/18-array-e-interação/array-interação.js
@@ -1,1 +1,21 @@
-const 
+const arrayCarros = ['Monza', 'Fiat', 'Uno'];
+
+arrayCarros.forEach((item, index, array) => {
+    item = item.toUpperCase();
+    console.log(item, index);
+});
+
+console.log(arrayCarros); // A array não muda após a alteração
+
+const li = document.querySelectorAll('li')
+
+//Quando for so um parâmetro e uma linha de cógico, não ´recisa dos parênteses nem das chaves
+li.forEach(i => i.classList.add('TESTE'));
+
+const cores = ['Azul', 'Preto', 'Vermelho']
+const coresModificadas = cores.map((item,index) => {
+    return item.toUpperCase()
+})
+
+console.log(coresModificadas);
+console.log(cores);
