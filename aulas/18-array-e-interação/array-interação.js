280ef79165e311d3ede507c9f88b776ee2658c74
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-23 14:10:50 -0300

    Update array-interação.js

--- aulas/18-array-e-interação/array-interação.js
+++ aulas/18-array-e-interação/array-interação.js
@@ -1,29 +1,29 @@
-const arrayCarros = ['Monza', 'Fiat', 'Uno'];
+const arrayCarros = ["Monza", "Fiat", "Uno"];
 
 arrayCarros.forEach((item, index, array) => {
-    item = item.toUpperCase();
-    console.log(item, index);
+  item = item.toUpperCase();
+  console.log(item, index);
 });
 
 console.log(arrayCarros); // A array não muda após a alteração
 
-const li = document.querySelectorAll('li')
+const li = document.querySelectorAll("li");
 
-//Quando for so um parâmetro e uma linha de cógico, não ´recisa dos parênteses nem das chaves
-li.forEach(i => i.classList.add('TESTE'));
+//Quando for só um parâmetro e uma linha de cógico, não precisa dos parênteses nem das chaves
+li.forEach((i) => i.classList.add("TESTE"));
 
-const cores = ['Azul', 'Preto', 'Vermelho']
-const coresModificadas = cores.map((item,index) => { 
-    return item.toUpperCase()
-})
+const cores = ["Azul", "Preto", "Vermelho"];
+const coresModificadas = cores.map((item, index) => {
+  return item.toUpperCase();
+});
 console.log(coresModificadas);
 
 // Usar o map quando for trabalhar com listas/arrays que vai alterar um valor
 const numeros = [10, 7, 33, 100];
 let result = 0;
 const numerosSomados = numeros.map((n) => {
-    result = result + n
-    return result
-})
+  result = result + n;
+  return result;
+});
 
 console.log(numerosSomados); // [10, 17, 50, 150]
