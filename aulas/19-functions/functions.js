b340efb8383e03c7594ccfa1583bebcd5a8efc13
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-30 14:35:23 -0300

    Update functions.js

--- aulas/19-functions/functions.js
+++ aulas/19-functions/functions.js
@@ -1,11 +1,27 @@
 // Toda função é criada apartir do construtor function, herdando seus métodos e propriedades.
-function somarNumeros(n1, n2) {
+function somarNumeros(n1, n2) { /// --> Parâmetos
     return n1 + n2;
 }
 // Sempre prestar atenção nos valores retornados da função
-console.log(somarNumeros(50, 10)); // Retornando um número
+// Retornando um número
+console.log(somarNumeros(50, 10)); // --> argumentos 
 
-function juntarLetras(letra1, letra2) {
+function juntarLetras(letra1, letra2, alfabeto) {
     return letra1 + letra2;
 }
 console.log(juntarLetras.length); // retorna o total de argumentos da função.
+console.log(juntarLetras.name); // retorna o nome da função
+
+
+// Toda função tem como referencial o Constructor Function, fazendo o this dela herdar os métodos e propriedades do construtor.
+// Com o .call() podemos mudar esse referencial, para um objeto por exemplo: 
+const pessoa = {
+    nome: "Willian",
+    idade: 18,
+}
+
+function nomePessoa(){
+    console.log(this.nome + " " + this.idade);
+}
+
+nomePessoa.call(pessoa) // Recebendo o referencial this.
