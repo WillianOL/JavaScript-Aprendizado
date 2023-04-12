0401e7eb7d9171c750cf7a4c6a98b4a7763f45c9
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-04-12 20:21:21 -0300

    Atualizações

---
+++ aulas/20-objects/objetos.js
@@ -0,0 +1,28 @@
+// Todo objeto é criado apartir do construtor Objetc, herdando seus métodos e propriedades.
+const pessoa = {
+    nome: "Willian",
+    idade: 18,
+};
+
+const pessoa2 = new Object({
+    nome: "Willian",
+    idade: 18,
+});
+
+console.log(pessoa.nome);
+console.log(pessoa2.idade);
+
+// Com o .create() pode-se fazer outro objeto herdar os métodos e propriedades que você criaou em outro objeto anterior
+const objeto = {
+    nome: "Claudio",
+    pegarPalavra(value) {
+        this.palavra = value
+        return this
+    },
+    juntar(){
+        return `${this.nome}, a sua palavra "${this.palavra}" é legal`
+    },
+}
+
+const willian = Object.create(objeto)
+console.log(willian.pegarPalavra("Pessego").juntar()); 
