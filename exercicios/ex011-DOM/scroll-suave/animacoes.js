54a8d761bccbe9d312e616c887be7a8b5384e310
Will Oliveira <williancontato67@gmail.com>
2023-04-27 19:41:30 -0300

    Atualizações

--- exercicios/ex011-DOM/scroll-suave/animacoes.js
+++ exercicios/ex011-DOM/scroll-suave/animacoes.js
@@ -1,3 +1,7 @@
-const h1 = document.querySelector('h1');
-
+const h1 = document.querySelector("h1");
 console.log(Object.prototype.toString.call(h1));
+
+// dataset - objeto do tipo DOMStringMap que armazena um conjunto de chaves/valores no HTML através de: data-
+// Facilita a interação entre o DOM e o JavaScript
+const div = document.querySelector("div");
+console.log(div.dataset.height);
