e77dd9ce4faa3f0748a7667ecfe7a2408a79b231
Will Oliveira <williancontato67@gmail.com>
2023-05-01 10:45:24 -0300

    Update animacoes.js

--- aulas/21-dataSet/animacoes.js
+++ aulas/21-dataSet/animacoes.js
@@ -4,6 +4,7 @@ console.log(Object.prototype.toString.call(h1));
 // dataset - objeto do tipo DOMStringMap que armazena um conjunto de chaves/valores no HTML através de: data-
 // Facilita a interação e manipulação do DOM com JavaScript
 const div = document.querySelector("div");
+console.log(div.dataset);
 console.log(div.dataset.height);
 
 // Mesma forma de selecionar, porém através de outros seletores(parâmetros)
