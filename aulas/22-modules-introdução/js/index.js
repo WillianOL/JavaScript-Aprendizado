cc4269d779fadf22e987927b25aa11d2b43b6074
Will Oliveira <williancontato67@gmail.com>
2023-05-04 20:33:14 -0300

    modificações

--- aulas/22-modules-introdução/js/index.js
+++ aulas/22-modules-introdução/js/index.js
@@ -1,11 +1,12 @@
 import iniScrollSuave from "./modules/scrollSuave.js";
 import initDescricao from "./modules/descricao.js";
 import initInfo from "./modules/mostrarInfo.js";
-import { somar, multiplicarPor5 } from "./modules/numbersOperation.js";
-// O ideal é importar só uma coisa como padrão
+import * as diversos from "./modules/exportacao.js";
 
 iniScrollSuave();
 initDescricao();
 initInfo();
-somar(50, 4);
-multiplicarPor5(5)
+diversos.somar(50, 4);
+diversos.multiplicarPor5(5)
+
+console.log(diversos.senha);
--- aulas/22-modules-introdução/js/modules/exportacao.js
+++ aulas/22-modules-introdução/js/modules/exportacao.js
@@ -5,3 +5,5 @@ export function somar(n1, n2){
 export function multiplicarPor5(n){
     console.log(n * 5);
 }
+
+export const senha = 392923882;
