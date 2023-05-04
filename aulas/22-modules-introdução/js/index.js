5e2ec52f5d5a03a860c10cd30b1d3f33e91a0f26
Will Oliveira <williancontato67@gmail.com>
2023-05-04 20:24:11 -0300

    Update index.js

--- aulas/22-modules-introdução/js/index.js
+++ aulas/22-modules-introdução/js/index.js
@@ -2,6 +2,7 @@ import iniScrollSuave from "./modules/scrollSuave.js";
 import initDescricao from "./modules/descricao.js";
 import initInfo from "./modules/mostrarInfo.js";
 import { somar, multiplicarPor5 } from "./modules/numbersOperation.js";
+// O ideal é importar só uma coisa como padrão
 
 iniScrollSuave();
 initDescricao();
