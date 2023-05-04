ba05ead0e0a0363b2dbe0847ec6b4967f039acbd
Will Oliveira <williancontato67@gmail.com>
2023-05-04 20:18:59 -0300

    Atualizações

--- aulas/22-modules-introdução/js/index.js
+++ aulas/22-modules-introdução/js/index.js
@@ -1,7 +1,10 @@
 import iniScrollSuave from "./modules/scrollSuave.js";
 import initDescricao from "./modules/descricao.js";
 import initInfo from "./modules/mostrarInfo.js";
+import { somar, multiplicarPor5 } from "./modules/numbersOperation.js";
 
 iniScrollSuave();
 initDescricao();
 initInfo();
+
+console.log(somar(5, 10));
---
+++ aulas/22-modules-introdução/js/modules/numbersOperation.js
@@ -0,0 +1,7 @@
+export function somar(n1, n2){
+    return n1 + n2
+}
+
+export function multiplicarPor5(n){
+    return n * 5
+}
