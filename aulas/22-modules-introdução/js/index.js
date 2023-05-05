a4d15670a608689cbec637ffd1f5221ac58f678f
Will Oliveira <williancontato67@gmail.com>
2023-05-05 19:52:16 -0300

    Update index.js

--- aulas/22-modules-introdução/js/index.js
+++ aulas/22-modules-introdução/js/index.js
@@ -4,7 +4,7 @@ import initInfo from "./modules/mostrarInfo.js";
 import * as diversos from "./modules/exportacao.js";
 
 // Quando o script está com o tipo de module(type="module"), ele entra no modo "use-strict". Ou seja, no modo estrito
-
+// Sendo assim: Não pode criar vareáveis globais, não pode deletar o que não é deletável, não pode mudar propriedade que está congelada, não pode declarar palavras chaves.
 iniScrollSuave();
 initDescricao();
 initInfo();
