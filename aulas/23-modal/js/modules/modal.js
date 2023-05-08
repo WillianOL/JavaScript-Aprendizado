542b901588b83fb3cb7d430d49b375997cbdbc77
Will Oliveira <williancontato67@gmail.com>
2023-05-08 19:49:18 -0300

    atualizações

--- aulas/23-modal/js/modules/modal.js
+++ aulas/23-modal/js/modules/modal.js
@@ -6,22 +6,24 @@ const containerModal = document.querySelector('[data-modal="conteiner"]')
 const btnAbrir = document.querySelector('[data-modal="abrir"]')
 const btnFechar = document.querySelector('[data-modal="fechar"]')
 
-
-function abrirModal(event) {
-    event.preventDefault();
-    containerModal.classList.add('ativo')
-}
-
-function fecharModal() {
-    containerModal.classList.remove('ativo')
-}
-
-function fecharClickForaModal(event) {
-    if(event.target === this){
-        fecharModal(event);
+if(containerModal && btnAbrir && btnFechar){
+    function abrirModal(event) {
+        event.preventDefault();
+        containerModal.classList.add('ativo')
     }
+    
+    function fecharModal() {
+        containerModal.classList.remove('ativo')
+    }
+    
+    function fecharClickForaModal(event) {
+        if(event.target === this){
+            fecharModal(event);
+        }
+    }
+    
+    btnAbrir.addEventListener("click", abrirModal);
+    btnFechar.addEventListener("click", fecharModal);
+    containerModal.addEventListener("click", fecharClickForaModal)
 }
 
-btnAbrir.addEventListener("click", abrirModal);
-btnFechar.addEventListener("click", fecharModal);
-containerModal.addEventListener("click", fecharClickForaModal)
