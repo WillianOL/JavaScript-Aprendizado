0f1157719c75c7cad8d919dd9cc6b60a0debf4e9
Will Oliveira <williancontato67@gmail.com>
2023-05-15 20:18:54 -0300

    modificações

--- aulas/23-modal/js/modules/tolltip.js
+++ aulas/23-modal/js/modules/tolltip.js
@@ -2,7 +2,7 @@ export default function initTooltip() {
     const todasAsTooltips = document.querySelectorAll("[data-tooltip]");
 
     todasAsTooltips.forEach((item) => {
-        item.addEventListener("mouseover", onMouseOver)
+        item.addEventListener("mouseover", onMouseOver);
     });
 
     function onMouseOver(event) {
@@ -13,26 +13,25 @@ export default function initTooltip() {
         onMouseLeave.tooltipBox = tooltipBox;
         onMouseLeave.element = this;
 
-        this.addEventListener("mouseleave", onMouseLeave)
+        this.addEventListener("mouseleave", onMouseLeave);
+        this.addEventListener("mousemove", onMouseMove);
     }
 
     const onMouseLeave = {
         tooltipBox: "",
         element: "",
-        handleEvent(){
+        handleEvent() {
             this.tooltipBox.remove();
-            this.element.removeEventListener("mouseleave", onMouseLeave)
-        }
-        
-    }
+            this.element.removeEventListener("mouseleave", onMouseLeave);
+        },
+    };
 
     function criarTooltipBox(element) {
         const tooltipBox = document.createElement("div");
         const text = element.getAttribute("aria-label");
-        tooltipBox.classList.add("tooltipBox")
-        tooltipBox.innerText = text
-        document.body.appendChild(tooltipBox)
-        return tooltipBox
+        tooltipBox.classList.add("tooltipBox");
+        tooltipBox.innerText = text;
+        document.body.appendChild(tooltipBox);
+        return tooltipBox;
     }
 }
-
