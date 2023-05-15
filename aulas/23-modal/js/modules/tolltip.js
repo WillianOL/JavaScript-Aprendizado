8179d03ed2536d2e063c6d54d8862186bfbd035c
Will Oliveira <williancontato67@gmail.com>
2023-05-15 20:08:37 -0300

    modificações

--- aulas/23-modal/js/modules/tolltip.js
+++ aulas/23-modal/js/modules/tolltip.js
@@ -7,5 +7,22 @@ todasAsTooltips.forEach((item) => {
 });
 
 function onMouseOver(event) {
-    console.log(event);
+    const tooltipBox = criarTooltipBox(this);
+    tooltipBox.style.top = event.pageY + "px";
+    tooltipBox.style.left = event.pageX + "px";
+
+    this.addEventListener("mouseleave", onMouseLeave)
+}
+
+function onMouseLeave() {
+    tooltipBox.remove();
+}
+
+function criarTooltipBox(element) {
+    const tooltipBox = document.createElement("div");
+    const text = element.getAttribute("aria-label");
+    tooltipBox.classList.add("tooltipBox")
+    tooltipBox.innerText = text
+    document.body.appendChild(tooltipBox)
+    return tooltipBox
 }
