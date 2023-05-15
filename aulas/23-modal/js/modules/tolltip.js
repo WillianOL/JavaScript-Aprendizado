b3312ab24d8411eb97c9ddcaa21608bd28ed9821
Will Oliveira <williancontato67@gmail.com>
2023-05-15 19:43:18 -0300

    Update tolltip.js

--- aulas/23-modal/js/modules/tolltip.js
+++ aulas/23-modal/js/modules/tolltip.js
@@ -1,19 +1,11 @@
 export default function initTooltip() {}
 
-const allTooltips = document.querySelectorAll("[data-tooltip]");
+const todasAsTooltips = document.querySelectorAll("[data-tooltip]");
 
-allTooltips.forEach((item) => {
-    item.addEventListener("mouseover", onMouseOver);
+todasAsTooltips.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver)
 });
 
 function onMouseOver(event) {
-    criarTooltipBox(this);
-}
-
-function criarTooltipBox(elemento) {
-    const tolltipBox = document.createElement("div");
-    const text = elemento.getAttribute("aria-label");
-    tolltipBox.classList.add("tooltip");
-    tolltipBox.innerText = text;
-    return tolltipBox;
+    console.log(event);
 }
