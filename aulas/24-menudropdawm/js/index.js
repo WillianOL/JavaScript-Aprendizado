52673a8b782aced869eb4c82ebc6c773c2c92a93
Will Oliveira <williancontato67@gmail.com>
2023-05-15 21:11:15 -0300

    atualizações

---
+++ aulas/24-menudropdawm/js/index.js
@@ -0,0 +1,11 @@
+import iniScrollSuave from "./modules/scrollSuave.js";
+import initDescricao from "./modules/descricao.js";
+import initInfo from "./modules/mostrarInfo.js";
+import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tolltip.js";
+
+initModal();
+iniScrollSuave();
+initDescricao();
+initInfo();
+initTooltip();
