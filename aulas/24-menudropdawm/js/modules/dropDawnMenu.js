c9ac71955653aaf4acb17b37e669d797295a90fa
Will Oliveira <williancontato67@gmail.com>
2023-05-22 07:24:48 -0300

    atualizações

--- aulas/24-menudropdawm/js/modules/dropDawnMenu.js
+++ aulas/24-menudropdawm/js/modules/dropDawnMenu.js
@@ -1,30 +1,19 @@
+import clickOutSide from "./outSideClick.js";
+
 export default function initDropDawnMenu() {
     const allDropMenus = document.querySelectorAll("[data-dropDawn]");
-
     allDropMenus.forEach((menu) => {
         // Forma de adicionar varios eventos a uma função
         ["click", "touchstart"].forEach((startEvent) => {
             menu.addEventListener(startEvent, handleClick);
         });
     });
-
     function handleClick(event) {
         event.preventDefault();
         this.classList.add("ativo")
         clickOutSide(this, () => {
             this.classList.remove("ativo")
         } )
-    }
-
-    function clickOutSide(element, callback) {
-        const html = document.documentElement;
-        html.addEventListener("click", handleOutSideClick)
-        function handleOutSideClick(event) {
-            if(!element.contains(event.target)){
-                html.removeEventListener("click", handleOutSideClick)
-                callback();
-            }
-        }
     }
 }
 
