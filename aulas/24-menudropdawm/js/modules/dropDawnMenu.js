8662650985fb7d9f1c1ecf6ff3f0cbf6245071a8
Will Oliveira <williancontato67@gmail.com>
2023-05-22 07:04:05 -0300

    Update dropDawnMenu.js

--- aulas/24-menudropdawm/js/modules/dropDawnMenu.js
+++ aulas/24-menudropdawm/js/modules/dropDawnMenu.js
@@ -1,28 +1,29 @@
-export default function initDropDawnMenu() {}
+export default function initDropDawnMenu() {
+    const allDropMenus = document.querySelectorAll("[data-dropDawn]");
 
-const allDropMenus = document.querySelectorAll("[data-dropDawn]");
-
-allDropMenus.forEach((menu) => {
-    // Forma de adicionar varios eventos a uma função
-    ["click", "touchstart"].forEach((startEvent) => {
-        menu.addEventListener(startEvent, handleClick);
+    allDropMenus.forEach((menu) => {
+        // Forma de adicionar varios eventos a uma função
+        ["click", "touchstart"].forEach((startEvent) => {
+            menu.addEventListener(startEvent, handleClick);
+        });
     });
-});
 
-function handleClick(event) {
-    event.preventDefault();
-    this.classList.add("ativo")
-    clickOutSide(this, () => {
-        this.classList.remove("ativo")
-    } )
-}
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add("ativo")
+        clickOutSide(this, () => {
+            this.classList.remove("ativo")
+        } )
+    }
 
-function clickOutSide(element, callback) {
-    const html = document.documentElement;
-    html.addEventListener("click", handleOutSideClick)
-    function handleOutSideClick(event) {
-        if(!element.contains(event.target)){
-            callback();    
+    function clickOutSide(element, callback) {
+        const html = document.documentElement;
+        html.addEventListener("click", handleOutSideClick)
+        function handleOutSideClick(event) {
+            if(!element.contains(event.target)){
+                callback();    
+            }
         }
     }
 }
+
