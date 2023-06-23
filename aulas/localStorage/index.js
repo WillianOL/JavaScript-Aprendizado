52b135d8a6cfd40a32dd91d2725d889ee20a584e
Will Oliveira <williancontato67@gmail.com>
2023-06-23 08:49:08 -0300

    Update index.js

--- aulas/localStorage/index.js
+++ aulas/localStorage/index.js
@@ -1,50 +1,50 @@
-const controles = document.getElementById("controles")
-const cssText = document.querySelector(".css")
-const btn = document.querySelector(".btn")
+const controles = document.getElementById("controles");
+const cssText = document.querySelector(".css");
+const btn = document.querySelector(".btn");
 
 const handleStyle = {
     element: btn,
-    backgroundColor(value){
-        this.element.style.backgroundColor = value
+    backgroundColor(value) {
+        this.element.style.backgroundColor = value;
     },
-    color(value){
-        this.element.style.color = value
+    color(value) {
+        this.element.style.color = value;
     },
-    height(value){
-        this.element.style.height = value + "px"
+    height(value) {
+        this.element.style.height = value + "px";
     },
-    width(value){
-        this.element.style.width = value + "px"
+    width(value) {
+        this.element.style.width = value + "px";
     },
-    border(value){
-        this.element.style.border = value
+    border(value) {
+        this.element.style.border = value;
     },
-    borderRadius(value){
-        this.element.style.borderRadius = value + "px"
+    borderRadius(value) {
+        this.element.style.borderRadius = value + "px";
     },
-    fontFamily(value){
-        this.element.style.fontFamily = value
+    fontFamily(value) {
+        this.element.style.fontFamily = value;
     },
-    fontSize(value){
-        this.element.style.fontSize = value + "px"
+    fontSize(value) {
+        this.element.style.fontSize = value + "px";
     },
-    texto(value){
-        this.element.innerText = value
-    }
-}
+    texto(value) {
+        this.element.innerText = value;
+    },
+};
 
 function pegarEstilos(event) {
     const nomeEvento = event.target.name;
     const valor = event.target.value;
 
-    handleStyle[nomeEvento](valor)
+    handleStyle[nomeEvento](valor);
     showCssStyle();
-    armazenarNoLocalStorage(nomeEvento, valor)
+    armazenarNoLocalStorage(nomeEvento, valor);
 }
 
 // Vai armazernar os valores no localStorege, com o nome e o valor.
-function armazenarNoLocalStorage(nome, valor){
-    localStorage[nome] = valor
+function armazenarNoLocalStorage(nome, valor) {
+    localStorage[nome] = valor;
 }
 
 function setValues() {
@@ -53,8 +53,9 @@ function setValues() {
 }
 setValues();
 
-function showCssStyle(){
-    cssText.innerHTML = "<span>" + btn.style.cssText.split("; ").join(";</span><span>")
+function showCssStyle() {
+    cssText.innerHTML =
+        "<span>" + btn.style.cssText.split("; ").join(";</span><span>");
 }
 
-controles.addEventListener("input", pegarEstilos)
+controles.addEventListener("input", pegarEstilos);
