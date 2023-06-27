453b8121618b90beecf64d208cd893374224a080
Will Oliveira <williancontato67@gmail.com>
2023-06-27 08:45:18 -0300

    promises

--- aulas/33-promises/index.js
+++ aulas/33-promises/index.js
@@ -1,13 +1,22 @@
 const promesa = new Promise((resolve, reject) =>{
-    const condicao = false;
+    const condicao = true;
 
+    // A maioria das promises vem acompanhadas de condições
     if(condicao){
-        resolve({});
+        setTimeout(() => {
+            resolve({nome: "Willian", idade: 18});
+        }, 1000)
     } else{
         reject(Error("Não funcionou"))
     }
 })
 
-promesa.then(() => {
-
+// O parâmetro do then é o valor que está dentro do resolve.(argumanto)
+let objetoFinal;
+promesa.then((resolucao) => {
+    return resolucao
+}).then((resultado2) => {
+    return resultado2.pais = "Grecia"
+}).then((final) => {;
 })
+// {nome: "Willian", idade: 18}
