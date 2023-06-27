b8dc1e10b4686915cb0943d3f4310057e01c6337
Will Oliveira <williancontato67@gmail.com>
2023-06-27 09:32:59 -0300

    Update index.js

--- aulas/33-promises/index.js
+++ aulas/33-promises/index.js
@@ -1,5 +1,5 @@
 const promesa = new Promise((resolve, reject) => {
-    const condicao = true;
+    const condicao = false;
 
     // A maioria das promises vem acompanhadas de condições
     if (condicao) {
@@ -20,9 +20,22 @@ const retorno = promesa
     .then((resultado2) => {
         resultado2.pais = "Brasil";
         return resultado2;
-    })
-    .catch((casoRejeitada) => {
+    },
+    casoRejeitada => {
         // catch vai ser executada caso a promise seja rejeitada
         console.log(casoRejeitada);
-    });
-// {nome: "Willian", idade: 18}
+    })
+    .finally(() => { // finally vai ser executado sempre, independente do resoltado da promise
+        console.log("Fim da promesa");
+    })
+
+const login = new Promise((resolve) => {
+    resolve("Usuário fez o login")
+})
+
+const dados = new Promise((resolve) => {
+    resolve("Dados carregados")
+})
+
+const carregouTudo = Promise.all([login, dados])
+console.log(carregouTudo);
