08ba83e78b1192189785f8833c5dce06acf3a409
Will Oliveira <williancontato67@gmail.com>
2023-07-10 20:54:28 -0300

    JSON

--- aulas/34-Fetch/index.js
+++ aulas/34-Fetch/index.js
@@ -1,4 +1,18 @@
-const documento = fetch("documento.txt")
-.then((resultado) => {
-    console.log(resultado);
+//Com a fetch podemos fazer requisições HTTP, e o que é retornado pra ela é uma PROMISE
+const documento = fetch("documento.txt") // Caminho do arquivo
+const conteudo = document.querySelector(".content")
+// O método text() retorna outra promesa, e o valor dela é o texto do arquivo HTTP 
+documento
+.then(resultado => resultado.text())
+.then(texto => {
+    conteudo.innerHTML = texto
+})
+
+console.log(documento);
+
+// API do banco de dados dos CEPS do Brasil
+fetch("https://viacep.com.br/ws/57690000/json/")
+.then(resultado => resultado.json())
+.then(cep => {
+    console.log(cep);
 })
