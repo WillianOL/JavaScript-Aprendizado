ecce10f3b77a0c868f97a3cb231e4752aa8ed01f
Will Oliveira <williancontato67@gmail.com>
2023-07-11 09:21:36 -0300

    Modificações

---
+++ aulas/35-fetch2/index.js
@@ -0,0 +1,29 @@
+const content = document.querySelector(".conteudo");
+
+const estiloCss = fetch("./style.css");
+
+estiloCss
+    .then((r) => r.text())
+    .then((css) => {
+        const tagStyle = document.createElement("style");
+        tagStyle.innerHTML = css;
+        content.appendChild(tagStyle);
+    });
+
+const imagem = fetch("constructorPerfil.png");
+
+imagem
+    .then((r) => r.blob())
+    .then((imagem) => {
+        const imgDOM = document.querySelector("img");
+        const imagemUrl = URL.createObjectURL(imagem);
+        imgDOM.src = imagemUrl;
+    });
+
+// Com o clone, a resposta é clonada. Usar clone caso você necessite transformar uma resposta em diferentes valores.
+const clone = fetch("https://viacep.com.br/ws/57690000/json/");
+clone.then((r) => {
+    const r2 = r.clone();
+    r.text().then((texto) => console.log(texto));
+    r2.json().then((objeto) => console.log(objeto));
+});
