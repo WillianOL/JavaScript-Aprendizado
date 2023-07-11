571835e5b8a6b4d1045aeb804aad7e4a567e5adf
Will Oliveira <williancontato67@gmail.com>
2023-07-11 08:16:42 -0300

    atualização

--- aulas/34-Fetch/index.js
+++ aulas/34-Fetch/index.js
@@ -11,13 +11,14 @@ documento
 console.log(documento);
 
 // API do banco de dados dos CEPS do Brasil
-fetch("https://viacep.com.br/ws/57690000/json/")
 // JSON é um tipo de formato de dados com JS. Possui a mesma sintax de um objeto JS
+fetch("https://viacep.com.br/ws/57690000/json/")
 .then(resultado => resultado.json())
 .then(cep => {
     conteudo.innerHTML = cep.localidade
 })
 
+// Implementando um CSS na div
 fetch("./style.css")
 .then(resultado => resultado.text())
 .then((css) => {
@@ -26,12 +27,14 @@ fetch("./style.css")
     conteudo.appendChild(style)
 })
 
+// Pegando o texto de um HTML
 const div = document.createElement("div")
 
 fetch("./login.html")
 .then(resultado => resultado.text())
 .then((texto) => {
     div.innerHTML = texto
-    const titulo = div.querySelector("h1").innerHTML
-    conteudo.innerHTML = titulo
+    const titulo = div.querySelector("h1").innerText
+    const paragrafo = div.querySelector("p").innerText
+    conteudo.innerHTML += `<br> ${titulo} <br> ${paragrafo}`
 })
