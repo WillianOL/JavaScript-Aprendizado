49f6c1bb9c41f87e84f742617eeff1a097b4ac20
Will Oliveira <williancontato67@gmail.com>
2023-07-10 21:53:57 -0300

    atualizações

--- aulas/34-Fetch/index.js
+++ aulas/34-Fetch/index.js
@@ -1,5 +1,5 @@
 //Com a fetch podemos fazer requisições HTTP, e o que é retornado pra ela é uma PROMISE
-const documento = fetch("documento.txt") // Caminho do arquivo
+const documento = fetch("./documento.txt") // Caminho do arquivo
 const conteudo = document.querySelector(".content")
 // O método text() retorna outra promesa, e o valor dela é o texto do arquivo HTTP 
 documento
@@ -12,7 +12,26 @@ console.log(documento);
 
 // API do banco de dados dos CEPS do Brasil
 fetch("https://viacep.com.br/ws/57690000/json/")
+// JSON é um tipo de formato de dados com JS. Possui a mesma sintax de um objeto JS
 .then(resultado => resultado.json())
 .then(cep => {
-    console.log(cep);
+    conteudo.innerHTML = cep.localidade
+})
+
+fetch("./style.css")
+.then(resultado => resultado.text())
+.then((css) => {
+    const style = document.createElement("style")
+    style.innerHTML = css;
+    conteudo.appendChild(style)
+})
+
+const div = document.createElement("div")
+
+fetch("./login.html")
+.then(resultado => resultado.text())
+.then((texto) => {
+    div.innerHTML = texto
+    const titulo = div.querySelector("h1").innerHTML
+    conteudo.innerHTML = titulo
 })
