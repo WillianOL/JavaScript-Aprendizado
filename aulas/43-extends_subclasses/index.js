9b3ed9dcbaf9b18dc2b52252cce0e46753e166cc
Will Oliveira <williancontato67@gmail.com>
2023-08-24 15:23:18 -0300

    Finalização da aula de Classes

--- aulas/43-extends_subclasses/index.js
+++ aulas/43-extends_subclasses/index.js
@@ -5,14 +5,51 @@ class Armario {
     abrirPorta() {
         return `As ${this.portas} portas se abriram`
     }
+    fecharPortas() {
+        return `As ${this.portas} portas se fecharam`
+    }
 }
 
-// Dessa forma pode-se fazer outra classe herdar os métodos e propriedades de outra.
+// Dessa forma pode-se fazer uma classe herdar os métodos e propriedades de outra.
 class ArmarioVerde extends Armario {
     abrirNovamente(){
         return `O armario verde de ${this.portas} portas abriu`
+    }
+    fecharPortas() {
+        return `O armario verde de ${this.portas} portas fechou`
     }
 }
 
 const armarioCincoPortas = new Armario(3)
 const outroArmario = new ArmarioVerde(5)
+console.log(armarioCincoPortas.abrirPorta());
+// O objeto outroArmario tem acesso aos métodos e propriedades do objeto armarioCincoPortas
+console.log(outroArmario.abrirPorta());
+
+// Os métodos podem se sobrescrever, mas não se sobrepoem. Os dois métodos ainda existem dentro das classes
+console.log(outroArmario.fecharPortas())
+console.log(armarioCincoPortas.fecharPortas())
+
+class Veiculo {
+    constructor(rodas) {
+        this.rodas = rodas;
+    }
+    acelerar(){
+        console.log("Acelerou")
+    }
+}
+// A palavra chave "super" serve para chamarmos um método da classe herdada dentro da que vai herdar. É a mesma coisa que colocar "carro.acelerar()"
+class Carro extends Veiculo {
+    constructor(rodas, velocidade, carteira) {
+        super(rodas)
+        this.velocidade = velocidade
+        this.carteira = carteira
+    }
+    acelerarMuito(){
+        super.acelerar()
+        console.log("Muito")
+    }
+}
+
+const carro = new Carro(4, "120Km/h", true)
+console.log(carro)
