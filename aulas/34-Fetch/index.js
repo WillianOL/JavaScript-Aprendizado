1bb35e6fb35906d3f5e8685604de49c4a24ff7a8
Will Oliveira <williancontato67@gmail.com>
2023-10-30 21:05:15 -0300

    Update index.js

--- aulas/34-Fetch/index.js
+++ aulas/34-Fetch/index.js
@@ -1,40 +1,40 @@
 //Com a fetch podemos fazer requisições HTTP, e o que é retornado pra ela é uma PROMISE
-const documento = fetch("./documento.txt") // Caminho do arquivo
-const conteudo = document.querySelector(".content")
-// O método text() retorna outra promesa, e o valor dela é o texto do arquivo HTTP 
+const documento = fetch('./documento.txt'); // Caminho do arquivo
+const conteudo = document.querySelector('.content');
+// O método text() retorna outra promesa, e o valor dela é o texto do arquivo HTTP
 documento
-.then(resultado => resultado.text())
-.then(texto => {
-    conteudo.innerHTML = texto
-})
+  .then((resultado) => resultado.text())
+  .then((texto) => {
+    conteudo.innerHTML = texto;
+  });
 
 console.log(documento);
 
 // API do banco de dados dos CEPS do Brasil
 // JSON é um tipo de formato de dados com JS. Possui a mesma sintax de um objeto JS
-fetch("https://viacep.com.br/ws/57690000/json/")
-.then(resultado => resultado.json())
-.then(cep => {
-    conteudo.innerHTML = cep.localidade
-})
+fetch('https://viacep.com.br/ws/57690000/json/')
+  .then((resultado) => resultado.json())
+  .then((cep) => {
+    conteudo.innerHTML = cep.localidade;
+  });
 
 // Implementando um CSS na div
-fetch("./style.css")
-.then(resultado => resultado.text())
-.then((css) => {
-    const style = document.createElement("style")
+fetch('./style.css')
+  .then((resultado) => resultado.text())
+  .then((css) => {
+    const style = document.createElement('style');
     style.innerHTML = css;
-    conteudo.appendChild(style)
-})
+    conteudo.appendChild(style);
+  });
 
 // Pegando o texto de um HTML
-const div = document.createElement("div")
+const div = document.createElement('div');
 
-fetch("./login.html")
-.then(resultado => resultado.text())
-.then((texto) => {
-    div.innerHTML = texto
-    const titulo = div.querySelector("h1").innerText
-    const paragrafo = div.querySelector("p").innerText
-    conteudo.innerHTML += `<br> ${titulo} <br> ${paragrafo}`
-})
+fetch('./login.html')
+  .then((resultado) => resultado.text())
+  .then((texto) => {
+    div.innerHTML = texto;
+    const titulo = div.querySelector('h1').innerText;
+    const paragrafo = div.querySelector('p').innerText;
+    conteudo.innerHTML += `<br> ${titulo} <br> ${paragrafo}`;
+  });
